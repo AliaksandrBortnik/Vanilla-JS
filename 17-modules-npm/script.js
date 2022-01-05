5775f302d10ae59ed41e61aed378284af09c412c
Alex Bortnik <bortnik.ar@gmail.com>
2022-01-05 21:42:18 +0300

    ES modules. Named, default, mix exports

--- 17-modules-npm/script.js
+++ 17-modules-npm/script.js
@@ -1,6 +1,20 @@
-import { cart, addToCart } from './shoppingCart.js';
+// 1. Import each individual
+//import {addToCart, totalPrice as price, tq} from './shoppingCart.js';
 
 console.log('Importing module')
 
-addToCart('lamp', 3);
-console.log(cart)
+// addToCart('bread', 5);
+// console.log(price, tq)
+
+// 2. Import all together as a namespace
+// import * as ShoppingCart from './shoppingCart.js';
+//
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice, ShoppingCart.tq)
+
+// 3. Import default
+import addToCart from "./shoppingCart.js";
+addToCart('pizza', 2);
+
+// 4. Possible mix of default and named
+// import addToCart, { totalPrice, tq } from "./shoppingCart.js";
