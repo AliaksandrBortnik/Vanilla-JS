ef720798ec9657cffd10159f6c12e4dde941cb06
Alex Bortnik <bortnik.ar@gmail.com>
2022-01-19 18:17:15 +0300

    Moved Bankist app to dedicated folder

--- 00-all-apps/bankist/script.js
+++ 00-all-apps/bankist/script.js
@@ -5,9 +5,22 @@
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -15,23 +28,22 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -64,7 +76,15 @@ let isSorted = false;
 
 const calcPrintBalance = function (account) {
   account.balance = account.movements.reduce((accum, mov) => accum += mov, 0);
-  labelBalance.textContent = account.balance + '€';
+  labelBalance.textContent = account.balance.toFixed(2) + '€';
+
+  const now = new Date();
+  const day = String(now.getDate()).padStart(2, '0');
+  const month = String(now.getMonth()).padStart(2, '0');
+  const year = now.getFullYear();
+  const hour = now.getHours();
+  const minute = now.getMinutes();
+  labelDate.textContent = `${day}/${month}/${year}, ${hour}:${minute}`;
 }
 
 const createUsernames = function (accounts) {
@@ -90,11 +110,17 @@ const displayMovements = function (movements, sort = false) {
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
         <div class="movements__date">3 days ago</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
     containerMovements.insertAdjacentHTML('afterbegin', rowTemplate);
+
+    // TODO: remove. simple test of reminder operation
+    const rows = Array.from(document.querySelectorAll('.movements__row'));
+    rows.forEach((element, i) => {
+      element.style.backgroundColor = i % 2 === 0 ? 'yellow' : 'orange';
+    });
   });
 }
 
@@ -103,16 +129,16 @@ const calcDisplaySummary = function (account) {
     .filter(mov => mov > 0)
     .reduce((accum, mov) => accum + mov, 0);
 
-  labelSumIn.textContent = `${sumIn}€`;
+  labelSumIn.textContent = `${sumIn.toFixed(2)}€`;
 
   const sumOut = account.movements
     .filter(mov => mov < 0)
     .reduce((accum, mov) => accum + mov, 0);
 
-  labelSumOut.textContent = `${sumOut}€`;
+  labelSumOut.textContent = `${sumOut.toFixed(2)}€`;
 
   const sumInterest = sumIn > Math.abs(sumOut) ? sumIn - Math.abs(sumOut) : 0;
-  labelSumInterest.textContent = `${sumInterest * account.interestRate / 100}€`;
+  labelSumInterest.textContent = `${(sumInterest * account.interestRate / 100).toFixed(2)}€`;
 };
 
 createUsernames(accounts);
@@ -209,7 +235,7 @@ btnClose.addEventListener('click', (e) => {
 
 btnLoan.addEventListener('click', (e) => {
   e.preventDefault();
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     currentAccount.movements.push(amount);
@@ -223,3 +249,8 @@ btnSort.addEventListener('click', (e) => {
   isSorted = !isSorted;
   displayMovements(currentAccount.movements, isSorted);
 });
+
+// TODO: remove later on. FAKE sign-in
+currentAccount = account1;
+updateUI(currentAccount);
+containerApp.style.opacity = '1';
