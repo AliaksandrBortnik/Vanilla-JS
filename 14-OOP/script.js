fedf0c7d6e2db7b3138ffca37b527f0b23ba381f
Alex Bortnik <bortnik.ar@gmail.com>
2022-01-23 16:56:56 +0300

    OOP - prototype challenge 1

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -33,3 +33,41 @@ console.log(Person.prototype.isPrototypeOf(ann)); // true
 
 Person.prototype.type = 'Human';
 console.log(ann.type); // from prototype, base field
+
+console.log(ann.hasOwnProperty('type')); // false because it's taken from prototype. not from object itself
+
+console.log(['a', 'b'].__proto__ === Array.prototype); // true
+// 5 -> Number(5)
+console.log(Number(5).__proto__ === Number.prototype); // true
+
+
+// Coding Challenge #1
+/*
+1. Use a constructor function to implement a Car. A car has a make and a speed property.
+The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+*/
+function Car(make, speed) {
+  this.make = make;
+  this.speed = speed;
+}
+
+Car.prototype.accelerate = function() {
+  this.speed += 10;
+  console.log(`Speed: ${this.speed}`);
+};
+
+Car.prototype.brake = function() {
+  this.speed -= 5;
+  console.log(`Speed: ${this.speed}`);
+}
+
+const bmw = new Car('BMW', 120);
+bmw.accelerate();
+
+const merc = new Car('Mercedes', 95);
+merc.brake();
