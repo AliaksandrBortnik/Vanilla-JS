ff66c021ed4ca7161a3c1cceaa1d7f9645c1fc2a
Alex Bortnik <bortnik.ar@gmail.com>
2022-01-26 22:03:10 +0300

    OOP - coding challenge #3

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -221,3 +221,37 @@ ford.speedUS = 100;
 console.log('Speed (km/h) of ford', ford.speed);
 ford.accelerate();
 console.log('Speed (km/h) of ford', ford.speed);
+
+
+// Coding Challenge #3
+
+// 1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car.
+// Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+function ElectricCar(make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge; // from 0 to 1
+}
+
+ElectricCar.prototype = Object.create(Car.prototype);
+ElectricCar.prototype.constructor = ElectricCar;
+
+// 2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+ElectricCar.prototype.chargeBattery = function(chargeTo) {
+  this.charge = chargeTo;
+};
+
+// 3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%.
+// Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+ElectricCar.prototype.accelerate = function() {
+  this.speed += 20;
+  this.charge -= 0.01;
+  console.log(`${this.make} going at ${this.speed} km/h, with a charge of ${this.charge * 100}%`);
+}
+
+// 4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%).
+// Notice what happens when you 'accelerate'! polymorphism
+const tesla = new ElectricCar('Tesla', 120, 0.23);
+console.log(tesla)
+tesla.accelerate();
+tesla.chargeBattery(100);
+tesla.brake();
