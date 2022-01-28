de21a66fe6be03e4f1aafb9080798449aeb1c31e
Alex Bortnik <bortnik.ar@gmail.com>
2022-01-29 00:48:34 +0300

    Mapty app: refactor to work with OOP

--- 15-mapty-app-geolocation/script.js
+++ 15-mapty-app-geolocation/script.js
@@ -11,69 +11,115 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map;
-let mapEvent;
+class App {
+  workouts = [];
+  #map;
+  #lastMapEvent;
 
-/*
-  The Geolocation API is accessed via a call to navigator.geolocation
-  this will cause the user's browser to ask them for permission to access their location data.
-  If they accept, then the browser will use the best available functionality on the device
-  to access this information (for example, GPS).
- */
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (geolocationPosition) {
-      console.log('Just got your location', geolocationPosition);
-      const {latitude, longitude} = geolocationPosition.coords;
-      const coords = [latitude, longitude];
-      const zoomLevel = 13;
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleActivityField);
+  }
 
-      map = L.map('map').setView(coords, zoomLevel);
-
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
-
-      map.on('click', (e) => {
-        form.classList.remove('hidden');
-        inputDistance.focus();
-        mapEvent = e;
-      });
-    },
-    function() {
-      console.warn('No success in getting your geolocation');
+  _getPosition() {
+    /*
+      The Geolocation API is accessed via a call to navigator.geolocation
+      this will cause the user's browser to ask them for permission to access their location data.
+      If they accept, then the browser will use the best available functionality on the device
+      to access this information (for example, GPS).
+    */
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (geoPosition) => this._loadMap(geoPosition.coords),
+        () => console.warn('No success in getting your geolocation')
+      );
     }
-  );
+  }
+
+  _loadMap({latitude, longitude}) {
+    const coords = [latitude, longitude];
+    const zoomLevel = 13;
+
+    this.#map = L.map('map').setView(coords, zoomLevel);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.#map);
+
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(e) {
+    this.#lastMapEvent = e;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleActivityField(e) {
+    if (!e.target.value) return;
+    // Switch between cycling and running type of activity.
+    inputCadence.parentElement.classList.toggle('form__row--hidden');
+    inputElevation.parentElement.classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+    clearForm();
+
+    const workout = {}; // TODO: implement
+    this.workouts.push(workout);
+
+    // Show marker
+    const coords = this.#lastMapEvent.latlng;
+    const pinCoords = [coords.lat, coords.lng];
+
+    L.marker(pinCoords)
+      .addTo(this.#map)
+      .bindPopup(L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false, // If open another popup, previous automatically closes
+        closeOnClick: false,
+        className: 'running-popup'
+      }))
+      .setPopupContent('Activity')
+      .openPopup();
+  }
 }
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+class Workout {
+  _id; // TODO: generate
 
-  // Clear form
+  constructor(distance, duration, coords) {
+    this._id = 'uniqueId';
+    this.distance = distance;
+    this.duration = duration;
+    this.coords = coords;
+  }
+}
+
+class Running extends Workout {
+  constructor(distance, duration, coords, cadence, pace) {
+    super(distance, duration, coords);
+    this.cadence = cadence;
+    this.pace = pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(distance, duration, coords, elevationGain, speed) {
+    super(distance, duration, coords);
+    this.elevationGain = elevationGain;
+    this.speed = speed;
+  }
+}
+
+function clearForm() {
   inputType.value = '';
   inputDistance.value = '';
   inputDuration.value = '';
   inputCadence.value = '';
+}
 
-  // Show marker
-  const pinCoords = [mapEvent.latlng.lat, mapEvent.latlng.lng];
-
-  L.marker(pinCoords)
-    .addTo(map)
-    .bindPopup(L.popup({
-      maxWidth: 250,
-      minWidth: 100,
-      autoClose: false, // If open another popup, previous automatically closes
-      closeOnClick: false,
-      className: 'running-popup'
-    }))
-    .setPopupContent('Activity')
-    .openPopup();
-});
-
-inputType.addEventListener('change', (e) => {
-  if (!e.target.value) return;
-  // Switch between cycling and running type of activity.
-  inputCadence.parentElement.classList.toggle('form__row--hidden');
-  inputElevation.parentElement.classList.toggle('form__row--hidden');
-});
+const app = new App();
