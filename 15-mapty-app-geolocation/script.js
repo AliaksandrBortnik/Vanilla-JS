c5c8a78641fc0137e66f498738b7e2e3fa531303
Alex Bortnik <bortnik.ar@gmail.com>
2022-01-28 23:29:07 +0300

    Mapty app: add activity form on map click

--- 15-mapty-app-geolocation/script.js
+++ 15-mapty-app-geolocation/script.js
@@ -11,6 +11,9 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map;
+let mapEvent;
+
 /*
   The Geolocation API is accessed via a call to navigator.geolocation
   this will cause the user's browser to ask them for permission to access their location data.
@@ -25,27 +28,16 @@ if (navigator.geolocation) {
       const coords = [latitude, longitude];
       const zoomLevel = 13;
 
-      const map = L.map('map').setView(coords, zoomLevel);
+      map = L.map('map').setView(coords, zoomLevel);
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map);
 
-      map.on('click', function(mapEvent) {
-        console.log(mapEvent);
-        const pinCoords = [mapEvent.latlng.lat, mapEvent.latlng.lng];
-
-        L.marker(pinCoords)
-          .addTo(map)
-          .bindPopup(L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false, // If open another popup, previous automatically closes
-            closeOnClick: false,
-            className: 'running-popup'
-          }))
-          .setPopupContent('Activity')
-          .openPopup();
+      map.on('click', (e) => {
+        form.classList.remove('hidden');
+        inputDistance.focus();
+        mapEvent = e;
       });
     },
     function() {
@@ -54,3 +46,34 @@ if (navigator.geolocation) {
   );
 }
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Clear form
+  inputType.value = '';
+  inputDistance.value = '';
+  inputDuration.value = '';
+  inputCadence.value = '';
+
+  // Show marker
+  const pinCoords = [mapEvent.latlng.lat, mapEvent.latlng.lng];
+
+  L.marker(pinCoords)
+    .addTo(map)
+    .bindPopup(L.popup({
+      maxWidth: 250,
+      minWidth: 100,
+      autoClose: false, // If open another popup, previous automatically closes
+      closeOnClick: false,
+      className: 'running-popup'
+    }))
+    .setPopupContent('Activity')
+    .openPopup();
+});
+
+inputType.addEventListener('change', (e) => {
+  if (!e.target.value) return;
+  // Switch between cycling and running type of activity.
+  inputCadence.parentElement.classList.toggle('form__row--hidden');
+  inputElevation.parentElement.classList.toggle('form__row--hidden');
+});
