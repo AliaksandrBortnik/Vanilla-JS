3dffa2807e471db4f2d8cdac834737ae7e95e461
Alex Bortnik <bortnik.ar@gmail.com>
2022-01-29 22:16:37 +0300

    Mapty app: support various activity types

--- 15-mapty-app-geolocation/script.js
+++ 15-mapty-app-geolocation/script.js
@@ -64,62 +64,112 @@ class App {
   }
 
   _newWorkout(e) {
+    const validInputs = (...inputs) => inputs.every(Number.isFinite);
+    const allPositive = (...inputs) => inputs.every(i => i > 0);
+
     e.preventDefault();
-    clearForm();
 
-    const workout = {}; // TODO: implement
-    this.workouts.push(workout);
-
-    // Show marker
     const coords = this.#lastMapEvent.latlng;
     const pinCoords = [coords.lat, coords.lng];
 
-    L.marker(pinCoords)
+    const activityType = inputType.value;
+    const distance = Number(inputDistance.value);
+    const duration = Number(inputDuration.value);
+
+    // Validation
+    if (!activityType) {
+      alert('Type of activity is required');
+      return;
+    }
+
+    let workout;
+
+    if (activityType === 'running') {
+      const cadence = Number(inputCadence.value);
+
+      if (!validInputs(distance, duration, cadence) ||
+          !allPositive(distance, duration, cadence)) {
+        alert('Values must be positive numbers.')
+        return;
+      }
+
+      workout = new Running(distance, duration, pinCoords, cadence);
+    }
+
+    if (activityType === 'cycling') {
+      const elevation = Number(inputElevation.value);
+
+      if (!validInputs(distance, duration, elevation) ||
+          !allPositive(distance, duration)) {
+        alert('Values must be positive numbers.')
+        return;
+      }
+
+      workout = new Cycling(distance, duration, pinCoords, elevation);
+    }
+
+    this.workouts.push(workout);
+    this._displayWorkoutPin(workout, activityType);
+    this._clearForm();
+  }
+
+  _displayWorkoutPin(workout) {
+    L.marker(workout.coords)
       .addTo(this.#map)
       .bindPopup(L.popup({
         maxWidth: 250,
         minWidth: 100,
         autoClose: false, // If open another popup, previous automatically closes
         closeOnClick: false,
-        className: 'running-popup'
+        className: `${workout.type}-popup`
       }))
-      .setPopupContent('Activity')
+      .setPopupContent('Activity') // TODO: fill content in pop-up
       .openPopup();
+  }
+
+  _clearForm() {
+    inputType.value = '';
+    inputDistance.value = '';
+    inputDuration.value = '';
+    inputCadence.value = '';
   }
 }
 
 class Workout {
-  _id; // TODO: generate
+  id = uuidv4();
+  date = new Date();
 
   constructor(distance, duration, coords) {
-    this._id = 'uniqueId';
-    this.distance = distance;
-    this.duration = duration;
+    this.distance = distance; // km
+    this.duration = duration; // min
     this.coords = coords;
   }
 }
 
 class Running extends Workout {
-  constructor(distance, duration, coords, cadence, pace) {
+  type = 'running';
+
+  constructor(distance, duration, coords, cadence) {
     super(distance, duration, coords);
     this.cadence = cadence;
-    this.pace = pace;
+  }
+
+  get pace() {
+    return this.duration / this.distance;
   }
 }
 
 class Cycling extends Workout {
-  constructor(distance, duration, coords, elevationGain, speed) {
+  type = 'cycling';
+
+  constructor(distance, duration, coords, elevationGain) {
     super(distance, duration, coords);
     this.elevationGain = elevationGain;
-    this.speed = speed;
   }
-}
 
-function clearForm() {
-  inputType.value = '';
-  inputDistance.value = '';
-  inputDuration.value = '';
-  inputCadence.value = '';
+  get speed() {
+    return this.distance / this.duration;
+  }
 }
 
 const app = new App();
