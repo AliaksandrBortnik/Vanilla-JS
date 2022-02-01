bc742f990a2a4252d1cba525beb44679dfc57eca
Alex Bortnik <bortnik.ar@gmail.com>
2022-02-01 16:59:10 +0300

    Asynchronous JS: coding challenge 2

--- 16-asynchronous-js/script.js
+++ 16-asynchronous-js/script.js
@@ -219,3 +219,66 @@ btn.addEventListener('click', whereAmI);
 // whereAmI(52.508, 13.381);
 // whereAmI(19.037, 72.873);
 // whereAmI(-33.933, 18.474);
+
+///////////////////////////////////////
+
+// Coding Challenge #2
+/*
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+*/
+
+// 1. Create a function 'createImage' which receives imgPath as an input.
+// This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute
+// to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and
+// resolve the promise. The fulfilled value should be the image element itself.
+// In case there is an error loading the image ('error' event), reject the promise.
+const imgContainer = document.querySelector('.images');
+
+const createImage = (imgPath) => {
+  return new Promise((resolve, reject) => {
+    const img = document.createElement('img');
+    img.src = imgPath;
+    img.addEventListener('error', reject);
+    img.addEventListener('load', () => {
+      imgContainer.append(img);
+      resolve(img);
+    });
+  });
+};
+
+let lastLoadedImg;
+
+createImage('./img/img-1.jpg')
+  // 2. Comsume the promise using .then and also add an error handler;
+  // 3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+  .then(img => {
+    lastLoadedImg = img;
+    return wait(2);
+  })
+  // 4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image
+  .then(_ => {
+    lastLoadedImg.style.display = 'none';
+    return createImage('./img/img-2.jpg');
+  })
+  // 5. After the second image has loaded, pause execution for 2 seconds again;
+  .then(img => {
+    lastLoadedImg = img;
+    return wait(2);
+  })
+  // 6. After the 2 seconds have passed, hide the current image.
+  .then(_ => {
+    lastLoadedImg.style.display = 'none';
+  })
+  .catch(console.error);
+
+
+
+
+
+
+
+
+
+
+
+
