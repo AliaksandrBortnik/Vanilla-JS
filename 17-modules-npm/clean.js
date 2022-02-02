c593ce523d9dab98559c1e383a26d13de131c363
Alex Bortnik <bortnik.ar@gmail.com>
2022-02-02 15:28:30 +0300

    Refactor - apply modern ES features for very old-style code

--- 17-modules-npm/clean.js
+++ 17-modules-npm/clean.js
@@ -1,4 +1,6 @@
-var budget = [
+'use strict';
+
+let budget = Object.freeze([ // High-level freeze only, no deep
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,58 +9,41 @@ var budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]) ;
 
-var limits = {
+// Immutable
+const allowedLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
+});
+
+const getUserLimit = (limits, user) => limits[user] || 0;
+
+// Pure function (no side-effects)
+const addExpense = (state, limits, value, description, user = 'jonas') => {
+  const convertedUser = user.toLowerCase();
+  const limit = getUserLimit(limits, convertedUser);
+  return value > limit ? state :
+    [...state, { value: -value, description, user: convertedUser }];
 };
 
-var add = function (value, description, user) {
-  if (!user) user = 'jonas';
-  user = user.toLowerCase();
+budget = addExpense(budget, allowedLimits, 10, 'Pizza 🍕');
+// budget = addExpense(budget, allowedLimits,100, 'Going to movies 🍿', 'Matilda');
+// budget = addExpense(budget, allowedLimits,200, 'Stuff', 'Jay');
 
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
-  } else {
-    lim = 0;
-  }
-
-  if (value <= lim) {
-    budget.push({ value: -value, description: description, user: user });
-  }
+const analyzeExpenses = (budget, limits) => {
+  return budget.map(entry =>
+    entry.value < -getUserLimit(limits, entry.user) ?
+      { ...entry, flag: 'limit' } : entry);
 };
-add(10, 'Pizza 🍕');
-add(100, 'Going to movies 🍿', 'Matilda');
-add(200, 'Stuff', 'Jay');
-console.log(budget);
+budget = analyzeExpenses(budget, allowedLimits);
 
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
-    } else {
-      lim = 0;
-    }
-
-    if (el.value < -lim) {
-      el.flag = 'limit';
-    }
-  }
+const getHighExpenses = (state, topLimit) => {
+  return state
+    .filter(entry => entry.value <= -topLimit)
+    .map(exp => exp.description.slice(-2))
+    .join(' / ');
 };
-check();
 
-console.log(budget);
-
-var bigExpenses = function (limit) {
-  var output = '';
-  for (var el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
-    }
-  }
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
-};
+const highestExpense = getHighExpenses(budget, 1000);
+console.warn('Highest expenses:', highestExpense);
