447808e94f64bae4b80194a8c34a01e9b13ac269
Alex Bortnik <bortnik.ar@gmail.com>
2022-02-09 00:19:08 +0300

    Forkify app - refactored to keep controller simple

--- 00-all-apps/forkify/src/js/controller.js
+++ 00-all-apps/forkify/src/js/controller.js
@@ -1,45 +1,15 @@
-
 import 'core-js/stable'; // Polyfill for common ES6 stuff
 import 'regenerator-runtime'; // Polyfill for async/await
 import * as model from "./model";
 import {recipeView} from "./views/recipeView";
 
-const recipeContainer = document.querySelector('.recipe');
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
-///////////////////////////////////////
-
-const showSpinner = function (parentElement) {
-  const template = `
-    <div class="spinner">
-      <svg>
-        <use href="${icons}#icon-loader"></use>
-      </svg>
-    </div>
-  `;
-  parentElement.innerHTML = '';
-  parentElement.insertAdjacentHTML('afterbegin', template);
-};
-
-const showRecipe = async function () {
+const controlRecipes = async function () {
   try {
     const recipeId = location.hash.slice(1);
-
     if (!recipeId) return;
 
-    showSpinner(recipeContainer);
-    // 1. Get data from API
+    recipeView.showSpinner();
     await model.loadRecipe(recipeId);
-    const recipe = model.state.recipe;
-
-    // 2. Prepare template to render
     recipeView.render(model.state.recipe);
   } catch (err) {
     alert(err);
@@ -47,4 +17,4 @@ const showRecipe = async function () {
 };
 
 ['load', 'hashchange'].forEach(event =>
-  window.addEventListener(event, showRecipe));
+  window.addEventListener(event, controlRecipes));
--- 00-all-apps/forkify/src/js/views/recipeView.js
+++ 00-all-apps/forkify/src/js/views/recipeView.js
@@ -2,17 +2,38 @@ import icons from 'url:../../img/icons.svg';
 
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
-  #recipeContainer = document.querySelector('.recipe');
-  #data;
+  #recipe;
 
   render(recipe) {
-    this.#data = recipe;
+    this.#recipe = recipe;
+    const template = this.#getTemplate(this.#recipe);
 
+    this.#clearView();
+    this.#parentElement.insertAdjacentHTML('beforeend', template);
+  }
+
+  showSpinner() {
     const template = `
+    <div class="spinner">
+      <svg>
+        <use href="${icons}#icon-loader"></use>
+      </svg>
+    </div>
+  `;
+    this.#parentElement.innerHTML = '';
+    this.#parentElement.insertAdjacentHTML('afterbegin', template);
+  };
+
+  #clearView() {
+    this.#parentElement.innerHTML = '';
+  }
+
+  #getTemplate() {
+    return `
       <figure class="recipe__fig">
-        <img src="${recipe.imageUrl}" alt="${recipe.title}" class="recipe__img" />
+        <img src="${this.#recipe.imageUrl}" alt="${this.#recipe.title}" class="recipe__img" />
         <h1 class="recipe__title">
-          <span>${recipe.title}</span>
+          <span>${this.#recipe.title}</span>
         </h1>
       </figure>
 
@@ -21,14 +42,14 @@ class RecipeView {
           <svg class="recipe__info-icon">
             <use href="${icons}#icon-clock"></use>
           </svg>
-          <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
+          <span class="recipe__info-data recipe__info-data--minutes">${this.#recipe.cookingTime}</span>
           <span class="recipe__info-text">minutes</span>
         </div>
         <div class="recipe__info">
           <svg class="recipe__info-icon">
             <use href="${icons}#icon-users"></use>
           </svg>
-          <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+          <span class="recipe__info-data recipe__info-data--people">${this.#recipe.servings}</span>
           <span class="recipe__info-text">servings</span>
 
           <div class="recipe__info-buttons">
@@ -60,7 +81,7 @@ class RecipeView {
       <div class="recipe__ingredients">
         <h2 class="heading--2">Recipe ingredients</h2>
         <ul class="recipe__ingredient-list">
-          ${recipe.ingredients.map(ing => `
+          ${this.#recipe.ingredients.map(ing => `
               <li class="recipe__ingredient">
                 <svg class="recipe__icon">
                   <use href="${icons}#icon-check"></use>
@@ -79,10 +100,10 @@ class RecipeView {
         <h2 class="heading--2">How to cook it</h2>
         <p class="recipe__directions-text">
           This recipe was carefully designed and tested by
-          <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
+          <span class="recipe__publisher">${this.#recipe.publisher}</span>. Please check out
           directions at their website.
         </p>
-        <a class="btn--small recipe__btn" href="${recipe.sourceUrl}" target="_blank">
+        <a class="btn--small recipe__btn" href="${this.#recipe.sourceUrl}" target="_blank">
           <span>Directions</span>
           <svg class="search__icon">
             <use href="${icons}#icon-arrow-right"></use>
@@ -90,9 +111,6 @@ class RecipeView {
         </a>
       </div>
     `;
-
-    this.#recipeContainer.innerHTML = ''; // TODO: remove
-    this.#recipeContainer.insertAdjacentHTML('beforeend', template);
   }
 }
 
