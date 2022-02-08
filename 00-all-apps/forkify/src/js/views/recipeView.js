7138025536eebebaca29b75cf030b29da124ce46
Alex Bortnik <bortnik.ar@gmail.com>
2022-02-09 00:45:23 +0300

    Forkify app - use Fraction lib to format quantity

--- 00-all-apps/forkify/src/js/views/recipeView.js
+++ 00-all-apps/forkify/src/js/views/recipeView.js
@@ -1,4 +1,5 @@
 import icons from 'url:../../img/icons.svg';
+import {Fraction} from 'fractional';
 
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
@@ -81,18 +82,7 @@ class RecipeView {
       <div class="recipe__ingredients">
         <h2 class="heading--2">Recipe ingredients</h2>
         <ul class="recipe__ingredient-list">
-          ${this.#recipe.ingredients.map(ing => `
-              <li class="recipe__ingredient">
-                <svg class="recipe__icon">
-                  <use href="${icons}#icon-check"></use>
-                </svg>
-                <div class="recipe__quantity">${ing.quantity || ''}</div>
-                <div class="recipe__description">
-                  <span class="recipe__unit">${ing.unit}</span>
-                  ${ing.description}
-                </div>
-              </li>
-            `).join('')}
+          ${this.#recipe.ingredients.map(this.#generateIngredientTemplate).join('')}
         </ul>
       </div>
 
@@ -110,6 +100,21 @@ class RecipeView {
           </svg>
         </a>
       </div>
+    `;
+  }
+
+  #generateIngredientTemplate(ing) {
+    return `
+      <li class="recipe__ingredient">
+        <svg class="recipe__icon">
+          <use href="${icons}#icon-check"></use>
+        </svg>
+        <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+        <div class="recipe__description">
+          <span class="recipe__unit">${ing.unit}</span>
+          ${ing.description}
+        </div>
+      </li>
     `;
   }
 }
